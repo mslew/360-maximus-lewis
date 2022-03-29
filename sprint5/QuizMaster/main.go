9cf374fd341a39d5809d0e153414d03a3f07cf15
Anthony Mastores <mastores27@gmail.com>
2022-03-29 14:12:24 -0500

    DB main

---
+++ sprint5/QuizMaster/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type DB struct {
+	Id        string `json:"Id"`
+	Name      string `json:"Name"`
+	NumQ      string `json:"NumQ"`
+	Questions string `json:"Questions"`
+	CorrectA  string `json:""`
+}
+
+// let's declare a global Articles array
+// that we can then populate in our main function
+// to simulate a database
+var QDB []DB
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homePage")
+}
+func returnAllDBs(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: returnDBs")
+	json.NewEncoder(w).Encode(QDB)
+}
+func returnSingleDB(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["id"]
+	//fmt.Fprintf(w, "Key: " + key)
+
+	for _, article := range QDB {
+		if article.Id == key {
+			json.NewEncoder(w).Encode(article)
+		}
+	}
+}
+
+func handleRequests() {
+	// creates a new instance of a mux router
+	myRouter := mux.NewRouter().StrictSlash(true)
+	// replace http.HandleFunc with myRouter.HandleFunc
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/all", returnAllDBs)
+
+	myRouter.HandleFunc("/DB/{id}", returnSingleDB)
+
+	// finally, instead of passing in nil, we want
+	// to pass in our newly created router as the second
+	// argument
+	log.Fatal(http.ListenAndServe(":10000", myRouter))
+}
+
+func main() {
+	QDB = []DB{
+		// DB{Id: "123", Name: "DB_H", Desc: "Article Description", Content: "Article Content"},
+		DB{Id: "123", Name: "DB_H", NumQ: "12", Questions: "Article Content", CorrectA: "{a,b,c,d,e}"},
+		DB{Id: "456", Name: "DB_S", NumQ: "6", Questions: "FILLER", CorrectA: "{b,x,d,x}"},
+		//DB{Id: "456", Name: "DB_S ", Desc: "Article Description", Content: "Article Content"},
+	}
+	handleRequests()
+}
