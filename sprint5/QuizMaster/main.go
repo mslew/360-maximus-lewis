664221aef79511d5adac203ffd62df058a7ad60c
Anthony Mastores <mastores27@gmail.com>
2022-04-01 10:39:12 -0500

    demo

--- sprint5/QuizMaster/main.go
+++ sprint5/QuizMaster/main.go
@@ -33,46 +33,6 @@ type ReturnFunction2 struct {
 	Options   []string
 }
 
-/*// let's declare a global Articles array
-// that we can then populate in our main function
-// to simulate a database
-var QDB []DB
-
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the HomePage!")
-	fmt.Println("Endpoint Hit: homePage")
-}
-func returnAllDBs(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Endpoint Hit: returnDBs")
-	json.NewEncoder(w).Encode(QDB)
-}
-func returnSingleDB(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	key := vars["id"]
-	//fmt.Fprintf(w, "Key: " + key)
-
-	for _, article := range QDB {
-		if article.id == key {
-			json.NewEncoder(w).Encode(article)
-		}
-	}
-}
-
-func handleRequests() {
-	// creates a new instance of a mux router
-	myRouter := mux.NewRouter().StrictSlash(true)
-	// replace http.HandleFunc with myRouter.HandleFunc
-	myRouter.HandleFunc("/", homePage)
-	myRouter.HandleFunc("/all", returnAllDBs)
-
-	myRouter.HandleFunc("/DB/{id}", returnSingleDB)
-
-	// finally, instead of passing in nil, we want
-	// to pass in our newly created router as the second
-	// argument
-	log.Fatal(http.ListenAndServe(":10000", myRouter))
-}*/
-
 func ReadFromJson() ReturnFunction1 {
 	databaseArray := DatabaseArray{}
 	content, err := os.Open("data.json")
