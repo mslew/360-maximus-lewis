2318bc301dff47ab7447fe93d395e8e606f191ad
AnthonyMastores <42610248+AnthonyMastores@users.noreply.github.com>
2022-04-18 20:58:24 -0500

    Add files via upload

---
+++ math-tutor/assets/js/app.js
@@ -0,0 +1,4 @@
+questionCreator(level) //creates random question based on level, keeps track of level and returns calculated answer
+submitAnswer(correctAnswer) //takes in user input answer, checks if correct -> green and next questions shown, if wrong red and allow 3 tries
+doMath(operator, a, b)  //calculates answer given operator, and two values a and b
+help() //shows game instrucions (possibly give dynamic hint)
